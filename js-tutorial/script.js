6c6e6be1eb15c1aad24f862a9ea475f16928d197
Dmytro <dimka670020040@gmail.com>
2025-12-04 21:04:45 +0100

    https://youtu.be/Yl7ULiRhhdU?t=6782 part 2

--- js-tutorial/script.js
+++ js-tutorial/script.js
@@ -37,10 +37,48 @@
 //   return balance;
 // };
 
-const calculateBalance = (income, expenses) => income - expenses;
+// const calculateBalance = (income, expenses) => income - expenses;
 
-const myBalance = calculateBalance(0, 2000);
-console.log(myBalance);
+// const myBalance = calculateBalance(0, 2000);
+// console.log(myBalance);
 
-const balanceEl = document.querySelector('.balance-number');
-balanceEl.addEventListener('click', () => {});
+// const balanceEl = document.querySelector('.balance-number');
+// balanceEl.insertAdjacentHTML('beforeend', '<span>Unavailable</span>');
+
+// if (500 > 200) {
+//   var amount = 5000;
+// }
+
+// console.log(amount);
+
+// const balanceNumber = 3000;
+// // const balanceText = 'Your balance is: ' + balanceNumber;
+// // console.log(balanceText);
+// const balanceText = `<li>
+// <span>${description}</span>
+// <span>${number}</span>
+// </li>`;
+// console.log(balanceText);
+
+// const balance = 2000;
+
+// balance < 0 ? console.log('Positive') : console.log('Negative');
+
+// const balance = 1000;
+
+// if (balance === 1000 && balance % 2 === 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+const incomes = [5000, 200, 10];
+
+// console.log(incomes[2]);
+
+// incomes.forEach((income) => console.log(income + 10));
+
+for (let i = 0; i < incomes.length; i++) {
+  // console.log(incomes[i]);
+  console.log(incomes[i]);
+}
