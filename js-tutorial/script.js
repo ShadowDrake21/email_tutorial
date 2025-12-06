4c6069ca42be321792f97034574955f39201a29e
Dmytro <dimka670020040@gmail.com>
2025-12-06 21:22:49 +0100

    https://www.youtube.com/watch?v=Yl7ULiRhhdU&t=6782s done

--- js-tutorial/script.js
+++ js-tutorial/script.js
@@ -1,84 +1,86 @@
-// const balance = 5000;
-// const description = 'Salary';
-// const newTransaction = true;
+const transactionsEl = document.querySelector('.transactions');
+const balanceNumberEl = document.querySelector('.balance-number');
+const numberIncomeEl = document.querySelector('.number--income');
+const numberExpensesEl = document.querySelector('.number--expenses');
+const formEl = document.querySelector('form');
+const inputDescriptionEl = document.querySelector('.input--description');
+const inputAmountEl = document.querySelector('.input--amount');
 
-// const balanceNumberEl = document.querySelector('.balance-number');
-// balanceNumberEl.textContent = 10000;
+const submitHandler = (event) => {
+  event.preventDefault();
+  const description = inputDescriptionEl.value;
+  const amount = +inputAmountEl.value;
 
-// console.log(1000);
+  const transactionEl = `
+   <li class="transaction ${
+     amount > 0 ? 'transaction--income' : 'transaction--expense'
+   }">
+            <span class="transaction__text">${description}</span>
+            <span class="transaction__amount">${
+              amount > 0 ? '+' : ''
+            }${amount}</span>
+            <button class="transaction__btn">X</button>
+          </li>`;
 
-// const text = 'Hello';
-// const salary = 2000;
-// const isExpense = false;
+  transactionsEl.insertAdjacentHTML('beforeend', transactionEl);
+  inputDescriptionEl.value = '';
+  inputAmountEl.value = '';
 
-// const expenses = [1000, 30, 25];
+  // unfocus form inputs
+  inputDescriptionEl.blur();
+  inputAmountEl.blur();
 
-// console.log(expenses[1]);
-// const translation = {
-//   description: 'Salary',
-//   amount: 5000,
-// };
+  if (amount > 0) {
+    const currentIncome = +numberIncomeEl.textContent;
+    const updatedIncome = currentIncome + amount;
+    numberIncomeEl.textContent = updatedIncome;
+  } else {
+    const currentExpenses = +numberExpensesEl.textContent;
+    const updatedExpenses = currentExpenses + -amount;
+    numberExpensesEl.textContent = updatedExpenses;
+  }
 
-// console.log(translation.amount);
+  // update balance
+  balanceNumberEl.textContent =
+    numberIncomeEl.textContent - numberExpensesEl.textContent;
+  if (balanceNumberEl.textContent < 0) {
+    balanceNumberEl.classList.add('balance--negative');
+  } else {
+    balanceNumberEl.classList.remove('balance--negative');
+  }
+};
 
-// const error = {
-//   description: 'Not found',
+formEl.addEventListener('submit', (event) => submitHandler(event));
 
-// }
+const clickHandler = (event) => {
+  // remove transaction item visually
+  const clickedEl = event.target.parentNode;
+  clickedEl.remove();
 
-// function calculateBalance(income, expenses) {
-//   const balance = income - expenses;
-//   return balance;
-// }
+  // update income or expenses
+  const amountEl = clickedEl.querySelector('.transaction__amount');
+  const amount = +amountEl.textContent;
 
-// function expression
-// var calculateBalance = function (income, expenses) {
-//   const balance = income - expenses;
-//   return balance;
-// };
+  if (amount > 0) {
+    const currentIncome = numberIncomeEl.textContent;
+    const updatedIncome = currentIncome - amount;
+    numberIncomeEl.textContent = updatedIncome;
+  } else {
+    const currentExpenses = numberExpensesEl.textContent;
+    const updatedExpenses = currentExpenses - -amount;
+    numberExpensesEl.textContent = updatedExpenses;
+  }
 
-// const calculateBalance = (income, expenses) => income - expenses;
+  // update balance
+  const income = numberIncomeEl.textContent - numberExpensesEl.textContent;
+  balanceNumberEl.textContent = income;
 
-// const myBalance = calculateBalance(0, 2000);
-// console.log(myBalance);
+  // make red if balance is negative
+  if (income < 0) {
+    balanceNumberEl.classList.add('balance--negative');
+  } else {
+    balanceNumberEl.classList.remove('balance--negative');
+  }
+};
 
-// const balanceEl = document.querySelector('.balance-number');
-// balanceEl.insertAdjacentHTML('beforeend', '<span>Unavailable</span>');
-
-// if (500 > 200) {
-//   var amount = 5000;
-// }
-
-// console.log(amount);
-
-// const balanceNumber = 3000;
-// // const balanceText = 'Your balance is: ' + balanceNumber;
-// // console.log(balanceText);
-// const balanceText = `<li>
-// <span>${description}</span>
-// <span>${number}</span>
-// </li>`;
-// console.log(balanceText);
-
-// const balance = 2000;
-
-// balance < 0 ? console.log('Positive') : console.log('Negative');
-
-// const balance = 1000;
-
-// if (balance === 1000 && balance % 2 === 0) {
-//   console.log(true);
-// } else {
-//   console.log(false);
-// }
-
-const incomes = [5000, 200, 10];
-
-// console.log(incomes[2]);
-
-// incomes.forEach((income) => console.log(income + 10));
-
-for (let i = 0; i < incomes.length; i++) {
-  // console.log(incomes[i]);
-  console.log(incomes[i]);
-}
+transactionsEl.addEventListener('click', (event) => clickHandler(event));
